baaf42f45975e57861eb2d0c3f562ba64d5c3076
Marce Melgar <m.melgaba@gmail.com>
2022-10-27 13:15:44 -0300

    caracter vacio

--- 1ra PreEntrega/script.js
+++ 1ra PreEntrega/script.js
@@ -3,9 +3,12 @@
 let personasRegistradas = parseInt(prompt("¿Cuantas personas registradas existen en tu Ecommerce?"))
 let acumularEdades = 0
 
-if (personasRegistradas < 0){
-    alert("Es imposible que el número de personas registradas sea negativo, recargue la página y vuelva a ingresar un número positivo")
+if (personasRegistradas = " ") {
+    alert("No ingresaste ningun número, vuelve a intentarlo recargando la página.")
 }
+else if (personasRegistradas < 0){
+    alert("Es imposible que el número de personas registradas sea negativo, recargue la página y vuelva a ingresar un número positivo")
+} 
 else if (personasRegistradas === 0) {
     alert("Al menos tiene que existir una persona registrada para saber el promedio de edad de tu Ecommerce")
 }
