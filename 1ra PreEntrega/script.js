96324093e5ec60df2bc5a3b25ac7f953e944b190
Marce Melgar <m.melgaba@gmail.com>
2022-10-31 16:31:21 -0300

    Todo Ok primera PreEntrega

--- 1ra PreEntrega/script.js
+++ 1ra PreEntrega/script.js
@@ -3,7 +3,7 @@
 let personasRegistradas = parseInt(prompt("¿Cuantas personas registradas existen en tu Ecommerce?"))
 let acumularEdades = 0
 
- while ((personasRegistradas <=0) || (isNaN(personasRegistradas))) {
+ while ((isNaN(personasRegistradas)) || (personasRegistradas <=0)) {
     personasRegistradas = parseInt(prompt("Dato equivocado, el número ingresado no puede ser negativo, igual a 0 o un caracter. Vuelva a intentarlo.¿Cuantas personas registradas existen en tu Ecommerce?"))
 } 
 
@@ -14,7 +14,7 @@ for (i = 1; i <= personasRegistradas; i++) {
         }
         else {
             alert("Ingrese un dato valido")
-            let edad = parseInt(prompt("Ingrese la edad de la persona nº " + i))
+            edad = parseInt(prompt("Ingrese la edad de la persona nº " + i))
         }
     }
     alert("El promedio de edad de todas las personas registradas en tu Ecommerce es: " + (acumularEdades/personasRegistradas).toFixed(2))
