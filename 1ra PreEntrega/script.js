f71412cae8d695d5e4d46aea07fb09877f234e1c
Marce Melgar <m.melgaba@gmail.com>
2022-10-31 16:16:01 -0300

    ajuste a ejercicio de 1ra PreEntrega

--- 1ra PreEntrega/script.js
+++ 1ra PreEntrega/script.js
@@ -3,18 +3,20 @@
 let personasRegistradas = parseInt(prompt("¿Cuantas personas registradas existen en tu Ecommerce?"))
 let acumularEdades = 0
 
-if (personasRegistradas < 0){
-    alert("Es imposible que el número de personas registradas sea negativo, recargue la página y vuelva a ingresar un número positivo")
+ while ((personasRegistradas <=0) || (isNaN(personasRegistradas))) {
+    personasRegistradas = parseInt(prompt("Dato equivocado, el número ingresado no puede ser negativo, igual a 0 o un caracter. Vuelva a intentarlo.¿Cuantas personas registradas existen en tu Ecommerce?"))
 } 
-else if (personasRegistradas === 0) {
-    alert("Al menos tiene que existir una persona registrada para saber el promedio de edad de tu Ecommerce")
-}
-else {
-    for (i = 1; i <= personasRegistradas; i++) {
-        let edad = parseInt(prompt("Ingrese la edad de la persona nº " + i))
-        acumularEdades = acumularEdades + edad 
-    }
 
-    console.log("El promedio de edad de todas las personas registradas en tu Ecommerce es: " + (acumularEdades/personasRegistradas).toFixed(2))
-}
+for (i = 1; i <= personasRegistradas; i++) {
+        let edad = parseInt(prompt("Ingrese la edad de la persona nº " + i))
+        if (edad>0) {
+        acumularEdades = acumularEdades + edad 
+        }
+        else {
+            alert("Ingrese un dato valido")
+            let edad = parseInt(prompt("Ingrese la edad de la persona nº " + i))
+        }
+    }
+    alert("El promedio de edad de todas las personas registradas en tu Ecommerce es: " + (acumularEdades/personasRegistradas).toFixed(2))
+    console.log("Gracias por utilizar este programa")
 
